d2f8d4aaea2e617bcbddd7b23325947e653c2c1c
Izat <tilekuuluizat@gmail.com>
2023-03-13 00:21:35 +0300

    testing

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,25 +1,25 @@
-import {INew} from '../types/news'
+// import {INew} from '../types/news'
 
 
-const newsCategories = ['Bussiness', "Sport", "Science", "Stars", "Tech", "Weather"]
-const description = 'The uppercase and lowercase will uppercase and lowercase text respectively, whereas capitalize utility will convert text to title-case. The normal-case utility can be used to preserve the original casing — typically to reset capitalization at different breakpoints.'
+// const newsCategories = ['Bussiness', "Sport", "Science", "Stars", "Tech", "Weather"]
+// const description = 'The uppercase and lowercase will uppercase and lowercase text respectively, whereas capitalize utility will convert text to title-case. The normal-case utility can be used to preserve the original casing — typically to reset capitalization at different breakpoints.'
 
 
-const newsData: INew[] = []
+// const newsData: INew[] = []
 
-for (let i = 0; i < 40; i++) {
-    const rand1 = Math.round(Math.random() * 5)
-    newsData.push(
-        {
-            id: i,
-            title: 'This Title is The Best? Isn\'t it? Just Watch and Enjoy',
-            author: 'Izat Musk',
-            description: description,
-            popularity: rand1 + 3,
-            category: newsCategories[rand1],
-            publishedAt: new Date().getFullYear().toString(),
-            img: ''
-        }
-    )
-}
-export default newsData;
+// for (let i = 0; i < 40; i++) {
+//     const rand1 = Math.round(Math.random() * 5)
+//     newsData.push(
+//         {
+//             id: i,
+//             title: 'This Title is The Best? Isn\'t it? Just Watch and Enjoy',
+//             author: 'Izat Musk',
+//             description: description,
+//             popularity: rand1 + 3,
+//             category: newsCategories[rand1],
+//             publishedAt: new Date().getFullYear().toString(),
+//             img: ''
+//         }
+//     )
+// }
+// export default newsData;
